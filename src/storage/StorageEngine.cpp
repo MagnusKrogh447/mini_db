cf1e2838cba49d6987dabfe4e3d84322b4671ddb
Magnus Krogh <magnuskrogh17@gmail.com>
2026-01-25 14:22:26 +0100

    Defined storage engine interface, with fake it implementaion

--- src/storage/StorageEngine.cpp
+++ src/storage/StorageEngine.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "StorageEngine.h"
+
+void StorageEngine::set(const std::string &key, const std::string &value) {}
+
+std::optional<std::string> StorageEngine::get(const std::string& key) {
+    return std::nullopt;
+}
+
+bool StorageEngine::remove(const std::string &key) {
+    return false;
+}
--- src/storage/StorageEngine.h
+++ src/storage/StorageEngine.h
@@ -5,8 +5,17 @@
 #ifndef MINI_DB_STORAGEENGINE_H
 #define MINI_DB_STORAGEENGINE_H
 
+#pragma once
+#include <string>
+#include <optional>
 
 class StorageEngine {
+public:
+    void set(const std::string& key, const std::string& value);
+
+    std::optional<std::string> get(const std::string& key);
+
+    bool remove(const std::string& key);
 };
 
 
